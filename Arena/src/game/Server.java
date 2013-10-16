935453b80ab434297b2a4f7deec65d91e9b882c4
balitkou <balitkou@mtu.edu>
2013-10-15 20:16:46 -0400

    Simple print.

--- Arena/src/game/Server.java
+++ Arena/src/game/Server.java
@@ -1,9 +1,27 @@
 package game;
+import java.net.*;
+import java.util.*;
+import java.io.*;
 
 public class Server {
 
+	private ServerSocket serverSocket;
+	private ArrayList<Socket> clientList;
+	private Socket s;
+	private PrintWriter pw;
 	
-	public static void main(String []args) {
-		System.out.println("Server online.");
+	public Server() {
+		try {
+			serverSocket = new ServerSocket(5378);
+			s = serverSocket.accept();
+			pw = new PrintWriter(s.getOutputStream(),true);
+			pw.print("Hello World.");
+			pw.close();
+			s.close();
+			serverSocket.close();
+		}
+		catch (Exception e) {
+			System.out.println("Error.");
+		}
 	}
 }
