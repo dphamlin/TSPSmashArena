6f838fca06fd9011d1bdd131cadb3551f4337d7d
Jacob Charles <jecharle@c28-0117-09.ad.mtu.edu>
2013-10-21 11:25:26 -0400

    Added ControlListener.java, which updates internal booleans of a Control object based on the actual key input.

---
+++ Arena/src/game/ControlListener.java
@@ -0,0 +1,56 @@
+package game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Update the buffers in a Controller object using actual key input
+ * 
+ * @author Jacob Charles
+ */
+public class ControlListener implements KeyListener {
+
+	private Controller c;
+	
+	/**
+	 * Initialize a ControlListener to a Controller
+	 * 
+	 * @param c
+	 * 		controller object to update
+	 */
+	public ControlListener(Controller c) {
+		this.c = c;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//Don't care, not using this method
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//Set relevant buffers to true
+		int k = e.getKeyCode();
+		if (k == KeyEvent.VK_UP) c.setbUp(true);
+		if (k == KeyEvent.VK_DOWN) c.setbDown(true);
+		if (k == KeyEvent.VK_LEFT) c.setbLeft(true);
+		if (k == KeyEvent.VK_RIGHT) c.setbRight(true);
+		if (k == KeyEvent.VK_Z) c.setbJump(true);
+		if (k == KeyEvent.VK_X) c.setbFire(true);
+		if (k == KeyEvent.VK_ESCAPE) c.setbStart(true);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//Set relevant buffers to false
+		int k = e.getKeyCode();
+		if (k == KeyEvent.VK_UP) c.setbUp(false);
+		if (k == KeyEvent.VK_DOWN) c.setbDown(false);
+		if (k == KeyEvent.VK_LEFT) c.setbLeft(false);
+		if (k == KeyEvent.VK_RIGHT) c.setbRight(false);
+		if (k == KeyEvent.VK_Z) c.setbJump(false);
+		if (k == KeyEvent.VK_X) c.setbFire(false);
+		if (k == KeyEvent.VK_ESCAPE) c.setbStart(false);
+	}
+
+}
