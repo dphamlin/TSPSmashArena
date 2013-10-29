63db202817f02f8e5861da5ccf02780c1582fef1
Jacob <jecharle@mtu.edu>
2013-10-28 23:37:38 -0400

    Changed the internal debug display to show a difference from ideal- when it reads 0 consistently, the problem is fixed.

--- Arena/src/game/View.java
+++ Arena/src/game/View.java
@@ -47,9 +47,9 @@ public class View extends JFrame {
 		this.setTitle("TSPArena: "+state.getMapName());
 		Image backBuffer = createImage(640, 480);
 		state.draw(backBuffer.getGraphics());
-		//test current dimensions
-		backBuffer.getGraphics().drawString(""+this.getContentPane().getWidth(), 20, 100);
-		backBuffer.getGraphics().drawString(""+this.getContentPane().getHeight(), 20, 120);
+		//illustrate how wrong the current dimensions are
+		backBuffer.getGraphics().drawString(""+(this.getContentPane().getWidth()-640), 20, 100);
+		backBuffer.getGraphics().drawString(""+(this.getContentPane().getHeight()-480), 20, 120);
 		this.getContentPane().getGraphics().drawImage(backBuffer, 0, 0, null);
 	}
 }
