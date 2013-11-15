98702211f45a79912a5906f625388f14d1131324
Dean Hamlin <dphamlin@c28-0117-03.ad.mtu.edu>
2013-11-15 16:16:49 -0500

    added concurrent client server communication

--- Arena/src/game/Server.java
+++ Arena/src/game/Server.java
@@ -1,13 +1,16 @@
 package game;
 
 import java.util.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.io.*;
 import java.net.*;
 
 import com.google.gson.*;
 
 public class Server {
-	
+
 	private ServerSocket serverSocket;
 	private ArrayList<Participant> participantList;
 	private int numberOfPlayers = 1;
@@ -18,6 +21,11 @@ public class Server {
 	private int activePlayerCount = 0;
 	private Message message;
 	private Boolean resultsSent;
+	private final Lock lock;
+	private final Condition done;
+	private int count = 0;
+	private ServerRunnable[] runner;
+	private Thread[] threads;
 
 	Server(int port) throws IOException {
 		serverSocket = new ServerSocket(port);
@@ -25,23 +33,28 @@ public class Server {
 		game = new ServerGameState();
 		json = new Gson();
 		timer = new StopWatch(20);
+		lock = new ReentrantLock();
+		done = lock.newCondition();
+		setCount(0);
+		runner = new ServerRunnable[4];
+		threads = new Thread[4];
 		setResultsSent(false);
 		setMessage(new Message(0,null));
 		System.out.println("Starting and listening on: "+getCurrentInetAddress()+":"+port);
 	}
-	
+
 	public void setNumberOfPlayers(int numberOfPlayers) { // Proper functioning only guaranteed for >=1 value.
 		this.numberOfPlayers = (numberOfPlayers >= 1)? numberOfPlayers : 1;
 	}
-	
+
 	public int getNumberOfPlayers() {
 		return numberOfPlayers;
 	}
-	
+
 	public ServerSocket getServerSocket() {
 		return serverSocket;
 	}
-	
+
 	public String getCurrentInetAddress(){
 		Enumeration<NetworkInterface> nets = null;
 		try {
@@ -54,13 +67,13 @@ public class Server {
 				try {//Find the interface that is active and host is communicating on
 					if(netint.isUp() && !netint.isPointToPoint() && !netint.isVirtual() && !netint.isLoopback()){
 						Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-				        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-				        	String inet = inetAddress.toString();
-				        	inet = inet.substring(1);//Find the address on the interface we want that is "real" and IPv4.
-				        	if(inet.substring(0,7).compareTo("169.254") != 0 && !inet.contains(":")){
-				        		return inet;
-				        	}
-				        }
+						for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+							String inet = inetAddress.toString();
+							inet = inet.substring(1);//Find the address on the interface we want that is "real" and IPv4.
+							if(inet.substring(0,7).compareTo("169.254") != 0 && !inet.contains(":")){
+								return inet;
+							}
+						}
 					}
 				} catch (SocketException e) {
 					System.err.println("Unable to get local address server is utilizing.");
@@ -72,19 +85,27 @@ public class Server {
 		}
 		return "Unknown.Address";//If we couldn't determine the address
 	}
-	
+
 	public ArrayList<Participant> getParticipantList() {
 		return participantList;
 	}
-	
+
 	public void setParticipantList(ArrayList<Participant> newParticipantList) {
 		participantList = newParticipantList;
 	}
-	
+
 	public ServerGameState getGameState() {
 		return game;
 	}
 	
+	public Gson getGson() {
+		return json;
+	}
+	
+	public void setGson(Gson json) {
+		this.json = json;
+	}
+
 	public StopWatch getTimer() {
 		return timer;
 	}
@@ -92,60 +113,60 @@ public class Server {
 	public void setTimer(StopWatch timer) {
 		this.timer = timer;
 	}
-	
+
 	// Reads new Controller objects from all participants in given list
 	public void readControllersFromAll(ArrayList<Participant> aParticipantList) {
-		for (Participant p: aParticipantList) {
-			if (p.isActive()) // Only try to read from active players; thread will be responsible for changing back to active on reconnect
-				try {
-					p.readController();
-				}
-				catch (IOException e) {
-					System.err.println("Participant disconnected on reading controller. Set to inactive. " + e.getMessage());
-					p.setActive(false);
-					setActivePlayerCount(getActivePlayerCount()-1);
-				}
+		getLock().lock();
+		try {
+			while(getCount() < getNumberOfPlayers())
+				done.await();
+			done.signalAll();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			getLock().unlock();
 		}
 	}
-	
+
 	public Message getMessage() {
 		return message;
 	}
-	
+
 	public void setMessage(Message message) {
 		this.message = message;
 	}
-	
+
 	// Writes the Server's message to all clients
 	public void writeMessageToAll(ArrayList<Participant> aParticipantList) {
+		getLock().lock();
+		try {
+			while(getCount() > getNumberOfPlayers())
+				done.await();
+			done.signalAll();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			getLock().unlock();
+		}
+	}
+
+	// Updates messageFromClient for all participants.  The Server can interpret messages from the client.
+	public void readMessageFromAll(ArrayList<Participant> aParticipantList) {
 		for (Participant p: aParticipantList) {
 			if (p.isActive())
 				try {
-					p.writeToClient(json.toJson(getMessage()));
+					p.readMessage();
 				}
 			catch (IOException e) {
-				System.err.println("Participant disconnected while writing message.  Set to inactive. " + e.getMessage());
+				System.err.println("Participant disconnected while reading message.  Set to inactive." + e.getMessage());
 				p.setActive(false);
 				setActivePlayerCount(getActivePlayerCount()-1);
 			}
 		}
 	}
-	
-	// Updates messageFromClient for all participants.  The Server can interpret messages from the client.
-	public void readMessageFromAll(ArrayList<Participant> aParticipantList) {
-			for (Participant p: aParticipantList) {
-				if (p.isActive())
-					try {
-						p.readMessage();
-					}
-					catch (IOException e) {
-						System.err.println("Participant disconnected while reading message.  Set to inactive." + e.getMessage());
-						p.setActive(false);
-						setActivePlayerCount(getActivePlayerCount()-1);
-					}
-			}
-	}
-	
+
 	// Writes the current game state to all clients as a JSON string
 	public void writeGameStateToAll(ArrayList<Participant> aParticipantList) { 
 		//System.out.println(json.toJson(getGameState().convert()));  //print content of each gamestate
@@ -155,11 +176,11 @@ public class Server {
 				try {
 					p.writeToClient(currentGameState);
 				}
-				catch (IOException e) {
-					System.err.println("Participant disconnected while writing game state.  Set to inactive. " + e.getMessage());
-					p.setActive(false);
-					setActivePlayerCount(getActivePlayerCount()-1);
-				}
+			catch (IOException e) {
+				System.err.println("Participant disconnected while writing game state.  Set to inactive. " + e.getMessage());
+				p.setActive(false);
+				setActivePlayerCount(getActivePlayerCount()-1);
+			}
 		}
 	}
 
@@ -167,7 +188,7 @@ public class Server {
 	public ArrayList<Participant> connectParticipants(int num) throws IOException {
 		Socket s = null;
 		ArrayList<Participant> newParticipantList = new ArrayList<Participant>();
-		
+
 		// Try to accept a connection; if successful, add a Participant with that connected socket
 		for (int i=0;i<num;i++) { 
 			try {
@@ -181,22 +202,25 @@ public class Server {
 				p.setPlayer(getGameState().addPlayer());
 				newParticipantList.add(p);
 				System.out.println("Player: "+(i+1)+" connected.");
+				runner[i] = new ServerRunnable(this, i);
+				threads[i] = new Thread(runner[i]);
 				setActivePlayerCount(getActivePlayerCount() + 1);
 			}
 		}
+		setCount(getNumberOfPlayers());
 		setNumberOfPlayers(newParticipantList.size());
 		getServerSocket().close(); // Stop accepting connections
 		return newParticipantList;
 	}
-	
+
 	public int getActivePlayerCount() {
 		return activePlayerCount;
 	}
-	
+
 	public void setActivePlayerCount(int activePlayerCount) {
 		this.activePlayerCount = activePlayerCount;
 	}
-	
+
 	public void applyAllControls(ArrayList<Participant> aParticipantList) {
 		for (Participant p: aParticipantList) {
 			if (p.isActive())
@@ -205,15 +229,43 @@ public class Server {
 				getGameState().suspendPlayer(p);
 		}
 	}
-	
+
 	public Boolean resultsSent() {
 		return resultsSent;
 	}
-	
+
 	public void setResultsSent(Boolean truthValue) {
 		this.resultsSent = truthValue;
 	}
-		
+
+	public Lock getLock() {
+		return lock;
+	}
+
+	public Condition getCondition() {
+		return done;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public void setCount(int c) {
+		this.count = c;
+	}
+
+	public Thread getThread(int i){
+		return this.threads[i];
+	}
+
+	public void runThreads() {
+		for(int i = 0; i < getNumberOfPlayers(); i++){
+			this.getThread(i).run();
+		}
+	}
+
+
+	// Main method
 	public static void main(String []args) {
 		int port = 5379;
 		int numberOfPlayers = 2;
@@ -228,7 +280,7 @@ public class Server {
 			Scanner inputScanner = new Scanner(System.in);
 			numberOfPlayers = inputScanner.nextInt();
 		} 
-		
+
 		Server theServer = null;
 		try {
 			theServer = new Server(port);
@@ -237,9 +289,9 @@ public class Server {
 			System.err.println("Could not start the server.");
 			System.exit(1);
 		}
-		
+
 		theServer.setNumberOfPlayers(numberOfPlayers);
-		
+
 		// Connect clients and adds them to the participantList
 		try {
 			theServer.setParticipantList(theServer.connectParticipants(theServer.getNumberOfPlayers()));
@@ -247,17 +299,19 @@ public class Server {
 		catch (Exception e) {
 			System.err.println("Error connecting client to server.\n");
 		}
-		
+
+		theServer.runThreads();
+
 		// All participants should connected; begin communication cycle
 		while (theServer.getActivePlayerCount() > 0) {
-			
+
 			theServer.getTimer().loopStart(); //log start time
-			
+
 			theServer.readControllersFromAll(theServer.getParticipantList()); // Reads updated controllers into all participants
 
 			theServer.applyAllControls(theServer.getParticipantList()); // Applies controls for all participants
 			theServer.getGameState().update(); // updates game state using game logic
-			
+
 			if (theServer.getGameState().getEnd() == 1 && !theServer.resultsSent()) {
 				theServer.getMessage().setNumber(1);
 				theServer.getMessage().setMessage(theServer.json.toJson(theServer.getGameState().getResults()));
@@ -270,12 +324,12 @@ public class Server {
 			}
 			theServer.writeMessageToAll(theServer.getParticipantList());
 			//theServer.writeGameStateToAll(theServer.getParticipantList());
-			
+
 			theServer.getTimer().loopRest(); //rest until loop end	
-			
+
 		}
 		System.out.println("All clients disconnected.  Server going offline.");
 		System.exit(0);
-		
+
 	}
 }
